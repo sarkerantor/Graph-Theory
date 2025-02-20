6a576a9ce63f330b1476adcfd185949cf2ade497
Antor Sarker <antor.s.cse10.bu@gmail.com>
2025-02-20 19:35:38 +0600

    Update Floyd_warshall.cpp

--- Floyd_warshall.cpp
+++ Floyd_warshall.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 const int inf=1e6;
 int main(){
-    vector<vector<int>>graph={{0,5,inf,10},
-                            {inf,0,3,inf},
-                            {inf,inf,0,1},
-                            {inf,inf,inf,0}};
+    vector<vector<int>>graph={{0,5,inf,9,1},
+                            {5,0,2,inf,inf},
+                            {inf,2,0,7,inf},
+                            {9,inf,7,0,2},
+                            {1,inf,inf,2,0}};
     int n=graph.size();
     vector<vector<int>>dist=graph;
     for(int k=0;k<n;k++){
