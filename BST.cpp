5f5a1fed57ab74646a059d80a8c6d46d908cb6c2
Antor Sarker <antor.s.cse10.bu@gmail.com>
2025-07-03 18:00:24 +0600

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,41 +1,81 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct Node{
+class Node{
+    public:
     int data;
-    Node *left,*right;
+    Node* left;
+    Node* right;
     Node(int val){
         data=val;
-        left=NULL;
-        right=NULL;
+        left=right=NULL;
     }
 };
-Node* insertbst(Node *root,int val){
-    if(root==NULL){
-        return new Node(val);
-    }
-    if(val<root->data){
-        root->left=insertbst(root->left,val);
-    }
-    else{
-        root->right=insertbst(root->right,val);
-    }
+static int idx=-1;
+Node* buildtree(vector<int>&v){
+    idx++;
+    if(v[idx]==-1)return NULL;
+    Node* root=new Node(v[idx]);
+    root->left=buildtree(v);
+    root->right=buildtree(v);
     return root;
 }
-void inorder(Node *root){
+//Preorder
+void Preorder(Node* root){
     if(root==NULL)return;
-    inorder(root->left);
     cout<<root->data<<" ";
-    inorder(root->right);
+    Preorder(root->left);
+    Preorder(root->right);
+}
+//Inorder
+void Inorder(Node* root){
+    if(root==NULL)return;
+    Inorder(root->left);
+    cout<<root->data<<" ";
+    Inorder(root->right);
+}
+//Postorder
+void Postorder(Node* root){
+    if(root==NULL)return;
+    Postorder(root->left);
+    Postorder(root->right);
+    cout<<root->data<<" ";
+}
+//Level order 
+void Levelorder(Node* root){
+    queue<Node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(q.size()>0){
+        Node* cur=q.front();
+        q.pop();
+        if(cur==NULL){
+            if(!q.empty()){
+                cout<<endl;
+                q.push(NULL);
+                continue;
+            }
+            else break;
+        }
+        cout<<cur->data<<" ";
+        if(cur->left!=NULL)q.push(cur->left);
+        if(cur->right!=NULL)q.push(cur->right);
+    }
 }
 int main(){
-    Node *root=NULL;
-    root=insertbst(root,5);
-    insertbst(root,1);
-    insertbst(root,6);
-    insertbst(root,12);
-    insertbst(root,41);
-    insertbst(root,11);
-    inorder(root);
+    vector<int> v = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    Node* root=buildtree(v);
+    //Create Binary Tree
+    cout<<root->data<<endl;
+    cout<<root->left->data<<endl;
+    cout<<root->right->data<<endl;
+    //For Traversal
+    Preorder(root);
+    cout<<endl;
+    Inorder(root);
+    cout<<endl;
+    Postorder(root);
+    cout<<endl;
+    Levelorder(root);
     cout<<endl;
     return 0;
 }
